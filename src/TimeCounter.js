0f3211ab255ecad8ac6d5c35a9dc92711edb7863
Vittorio Nardone <vittorio.nardone@gmail.com>
2020-08-29 10:23:11 +0200

    Frontend code linting

--- src/TimeCounter.js
+++ src/TimeCounter.js
@@ -1,10 +1,10 @@
-import { useElapsedTime } from 'use-elapsed-time'
+import {useElapsedTime} from 'use-elapsed-time';
 
 const TimeCounter = () => {
-  const isPlaying = true
-  const { elapsedTime } = useElapsedTime(isPlaying)
+  const isPlaying = true;
+  const {elapsedTime} = useElapsedTime(isPlaying);
 
-  return elapsedTime.toFixed(2)
-}
+  return elapsedTime.toFixed(2);
+};
 
 export default TimeCounter;
